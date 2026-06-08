0752fe4cc3470cf006d5def70fe66404b145f9fd
Quark <lucas.robotron.98@gmail.com>
2026-06-08 20:23:07 -0300

    fix

--- src/numbers.h
+++ src/numbers.h
@@ -8,12 +8,12 @@ typedef uint32_t  u32;
 typedef int32_t   i32;
 typedef int64_t   i64;
 
-u64 GetFloatNumberBytes(double number);
-u64 GetFloatNumberMantissa(const u64 numberBytes);
-i32 GetFloatNumberBiasExponent(const u64 numberBytes);
-i32 GetFloatNumberExponent(const u64 numberBytes);
-i32 GetFloatNumberSignal(const u64 numberBytes);
-double CreateFloatNumber(const u64 mantissa, const u32 biasExp, const u32 signal);
+inline u64 GetFloatNumberBytes(double number);
+inline u64 GetFloatNumberMantissa(const u64 numberBytes);
+inline i32 GetFloatNumberBiasExponent(const u64 numberBytes);
+inline i32 GetFloatNumberExponent(const u64 numberBytes);
+inline i32 GetFloatNumberSignal(const u64 numberBytes);
+inline double CreateFloatNumber(const u64 mantissa, const u32 biasExp, const u32 signal);
 void NormalizeFloatNumber(double number, double* pNormalized, i32* pExponent, i32* pSignal);
 
 #endif
